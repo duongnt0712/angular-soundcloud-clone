ff19624aa59fe3a1aafd6cef7397e121ab79658b
duongnt0712 <ntduong071200@gmail.com>
2022-07-04 22:30:19 +0700

    modify main-page

--- src/app/pages/main-page.module.ts
+++ src/app/pages/main-page.module.ts
@@ -2,13 +2,15 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { MainPageRoutingModule } from './main-page-routing.module';
-import { TabViewModule } from 'primeng/tabview';
-
-import {MenubarModule} from 'primeng/menubar';
 import { MainPageComponent } from './main-page.component';
 
+import { TabViewModule } from 'primeng/tabview';
+import {MenubarModule} from 'primeng/menubar';
+import {ButtonModule} from 'primeng/button';
+
 const pModules = [
-  MenubarModule
+  MenubarModule,
+  ButtonModule,
 ]
 
 @NgModule({
